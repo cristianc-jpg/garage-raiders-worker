000ece7fcb44b36ac60299da7da9726ce956b418
cristianc-jpg <cristian.c@garageraiders.com>
2025-09-22 14:38:18 -0500

    Update worker.ts

--- worker.ts
+++ worker.ts
@@ -1,14 +1,1 @@
-import { Worker } from '@temporalio/worker';
 
-async function run() {
-  const worker = await Worker.create({
-    workflowsPath: require.resolve('./workflows'), // point to your workflows
-    taskQueue: process.env.TEMPORAL_TASK_QUEUE || 'sms-orchestrator',
-  });
-  await worker.run();
-}
-
-run().catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
